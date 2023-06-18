ead80d3ee925d871db79f274ddd2b0bedfec2ce8
Lijiangnan Tian <lijiangn@ualberta.ca>
2023-06-17 19:02:09 -0600

    Initial Commit

---
+++ 傳統字形󠄁/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  傳統字形󠄁
+//
+//  Created by Tian on 2023-06-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 傳統字形󠄁/______App.swift
@@ -0,0 +1,17 @@
+//
+//  ______App.swift
+//  傳統字形󠄁
+//
+//  Created by Tian on 2023-06-17.
+//
+
+import SwiftUI
+
+@main
+struct ______App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
